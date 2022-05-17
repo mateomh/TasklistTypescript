019d0614369232e1a3b66264888681577096c2e7
Mateo mojica <mmojicah.84@gmail.com>
2022-05-16 20:43:26 -0500

    Create the task model to use in the app and create a state related to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Task } from './models/task_model';
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-
+  const [tasks, setTasks] = useState<Task[]>([]);
+  
   return (
     <div className="App">
       <h1 className='heading'>Taskify</h1>
