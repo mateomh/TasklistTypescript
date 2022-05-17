3357d03f79fa3cf44e74fab3a0772a048ff3302d
Mateo mojica <mmojicah.84@gmail.com>
2022-05-16 20:26:16 -0500

    Add classes to the css to style them later

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,9 @@
 const InputField: React.FC = () => {
   return(
-    <form className="task_input">
+    <form className="task_input_form">
       <input 
         type="text"
-        className="task"
+        className="task_input"
         placeholder="Enter a task" 
       />
       <button className="task_button">Go</button>
