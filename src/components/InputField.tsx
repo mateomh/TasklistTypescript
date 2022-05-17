abae931a91fcbf87f8d770ec4a0bb2b471c08416
Mateo mojica <mmojicah.84@gmail.com>
2022-05-16 20:37:33 -0500

    Add props and interface to the input component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,23 @@
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo}) => {
   return(
     <form className="task_input_form">
       <input 
         type="text"
         className="task_input"
-        placeholder="Enter a task" 
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       />
-      <button className="task_button">Go</button>
+      <button
+        className="task_button"
+      >
+        Go
+      </button>
     </form>
   );
 }
