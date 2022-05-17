5491a5d591f6a40b2a34681608b827bf73ea91d7
Mateo mojica <mmojicah.84@gmail.com>
2022-05-16 20:57:26 -0500

    Create the task list and link it to the input component

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,23 @@ import { Task } from './models/task_model';
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [tasks, setTasks] = useState<Task[]>([]);
-  
+
+  const handleAddTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(todo) {
+      setTasks([...tasks, {id: Math.random(), text: todo, isDone: false}]);
+      setTodo("");
+    }
+  }
+
   return (
     <div className="App">
       <h1 className='heading'>Taskify</h1>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField
+        todo={todo}
+        setTodo={setTodo}
+        addTask={handleAddTask}
+        />
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,12 @@
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  addTask: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({todo, setTodo}) => {
+const InputField: React.FC<Props> = ({todo, setTodo, addTask}) => {
   return(
-    <form className="task_input_form">
+    <form className="task_input_form" onSubmit={(e) => addTask(e)}>
       <input 
         type="text"
         className="task_input"
